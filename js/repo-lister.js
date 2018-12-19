098aab1e821ab1379a31c743c7313dd47299dd04
Eamonn de Leastar <edeleastar@wit.ie>
2018-12-19 16:41:57 +0000

    first request to github - log success or fail

--- js/repo-lister.js
+++ js/repo-lister.js
@@ -1,4 +1,17 @@
-$('#fetch-btn').click(function () {
-    var githubId = $('#github-id').val();
-    console.log(githubId);
+$("#fetch-btn").click(function() {
+  var githubId = $("#github-id").val();
+  console.log(githubId);
+
+  $.ajax({
+    dataType: "json",
+    url: "https://api.github.com/users/" + githubId + "/repos",
+    success: function(data) {
+      console.log("success");
+    },
+
+    error: function(err) {
+      console.log("fail");
+      console.log(err.statusText);
+    }
+  });
 });
